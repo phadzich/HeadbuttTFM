966dc07f86a3a173ffb69d6fdb85f8be29226034
phadzich <phadzich@gmail.com>
2025-02-12 16:07:42 -0500

    Bug fixes

--- Assets/Scripts/PlayerBounce.cs
+++ Assets/Scripts/PlayerBounce.cs
@@ -18,8 +18,19 @@ public class PlayerBounce : MonoBehaviour
     float headbuttCooldown;
     [SerializeField]
     bool headbuttOnCooldown;
+    [SerializeField]
+    float blockLockdownRange;
+
+    [SerializeField]
+    string bounceDirection;
+
+    [SerializeField]
+    bool timedHeadbutt;
 
     public PlayerStates playerStates;
+
+    [SerializeField]
+    BlockData blockBelow;
 
     private void Start()
     {
@@ -30,53 +41,122 @@ public class PlayerBounce : MonoBehaviour
 
     void Update()
     {
-        CheckForHeadbuttDistance();
-        UpdateHeadbuttCooldown();
-    }
-
-    private void CheckForHeadbuttDistance()
-    {
-        // Ray start position
-        Vector3 origin = transform.position;
-
-        // Direction downwards
-        Vector3 direction = Vector3.down;
-
-
-        // Draw a green ray in the Scene view
-        Debug.DrawRay(origin, direction * headbuttRange, Color.red);
-
-        // Perform the raycast
-        if (Physics.Raycast(origin, direction, out RaycastHit hit, headbuttRange)&&!headbuttOnCooldown)
+        CheckForBlockBelow();
+        CheckBounceDirection();
+        if (bounceDirection == "DOWN")
         {
-            //Debug.Log("Headbutt: " + hit.collider.name);
-            ToggleHeadbuttIndicator(true);
-            playerStates.EnterHeadbuttState();
+
+            CheckForHeadbuttDistance();
+            CheckForBounceDistance();
         }
         else
         {
             ToggleHeadbuttIndicator(false);
             playerStates.EnterIdleState();
         }
+
+        UpdateHeadbuttCooldown();
+    }
+
+    private void CheckBounceDirection()
+    {
+        if(rb.linearVelocity.y >= 0)
+        {
+            bounceDirection = "UP";
+        }
+        else
+        {
+            bounceDirection = "DOWN";
+        }
+    }
+
+    private void CheckForBlockBelow()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+        Debug.DrawRay(origin, direction * 5f, Color.yellow);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, 5f))
+        {
+
+            blockBelow = hit.collider.GetComponent<BlockData>();
+        }
+    }
+
+    private void CheckMovementLock()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+        Debug.DrawRay(origin, direction * blockLockdownRange, Color.yellow);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, blockLockdownRange) && bounceDirection == "DOWN")
+        {
+
+            GameManager.instance.levelMovement.movementLocked = true;
+        }
+        else
+        {
+            GameManager.instance.levelMovement.movementLocked = false;
+        }
+    }
+
+
+
+    private void CheckForBounceDistance()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+        float _groundDistance = .1f;
+        Debug.DrawRay(origin, direction * _groundDistance, Color.red);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, _groundDistance))
+        {
+            BounceUp();
+        }
+    }
+
+    private void CheckForHeadbuttDistance()
+    {
+
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+        Debug.DrawRay(origin, direction * headbuttRange, Color.red);
+
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, headbuttRange)&&!headbuttOnCooldown)
+        {
+            //Debug.Log("Headbutt: " + hit.collider.name);
+            ToggleHeadbuttIndicator(true);
+            playerStates.EnterHeadbuttState();
+        }
+
     }
 
     private void ToggleHeadbuttIndicator(bool _condition)
     {
         inHeadbuttRange = _condition;
-        GameManager.instance.levelMovement.movementLocked = _condition;
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log(other);
-        BounceUp();
-        other.GetComponent<BlockData>().GetHit();
     }
 
     private void BounceUp()
     {
+        Debug.Log("BOUNCE!");
         rb.linearVelocity = Vector3.zero;
-        rb.AddForce(Vector3.up* jumpForce);
+        if (!timedHeadbutt)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+        blockBelow.GetHit();
+        timedHeadbutt = false;
+    }
+
+    private void HeadbuttUp()
+    {
+        Debug.Log("HEADBUTT!");
+        rb.linearVelocity = Vector3.zero;
+        rb.AddForce(Vector3.up * headbuttPower);
+        blockBelow.GetHit();
+        RestartHeadbuttCooldown();
     }
 
     public void Headbutt(InputAction.CallbackContext context)
@@ -85,10 +165,15 @@ public class PlayerBounce : MonoBehaviour
         {
             if (inHeadbuttRange && !headbuttOnCooldown)
             {
-                Debug.Log("HEADBUTT!");
-                rb.linearVelocity = Vector3.zero;
-                rb.AddForce(Vector3.up * headbuttPower);
-                RestartHeadbuttCooldown();
+                timedHeadbutt = true;
+                Debug.Log("CORRECT!");
+                HeadbuttUp();
+                timedHeadbutt = false;
+            }
+            else
+            {
+                timedHeadbutt = false;
+                Debug.Log("MISSED!");
             }
         }
 
@@ -106,12 +191,12 @@ public class PlayerBounce : MonoBehaviour
             headbuttOnCooldown = false;
         }
 
-
     }
 
     private void RestartHeadbuttCooldown()
     {
         timeSinceLastHeadbutt = 0;
+
     }
 
     }
