dd6a38fc620f4bab15e4abdccb03fcda3597bf55
María José Burguete Euán <marijo.mjbe@gmail.com>
2025-06-10 23:55:59 -0600

    Feedback button function
    Ahora el boton de feedback lleva al jugador al google forms

--- Assets/UIPanelConfig.cs
+++ Assets/UIPanelConfig.cs
@@ -14,4 +14,6 @@ public class UIPanelConfig : MonoBehaviour
     {
         GameManager.Instance.PauseGame(!unpauseOnClose);
     }
+
+    public void OpenGoogleForm() { Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSc1K6Mk7Lu5aLRvEJxpBBAuJGeOzEsi3qPTpSMoP6H5tCkrRw/viewform?usp=dialog"); }
 }
